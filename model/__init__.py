8000716ead6a0fe87c23094767c7d0a240069a5b
Peter Racioppo <59070819+PCR-git@users.noreply.github.com>
2025-06-23 20:41:12 -0700

    Delete model directory

--- model/__init__.py
+++
@@ -1,8 +0,0 @@
-from .model_utils import compute_lambda_h, predict_multiple_steps
-from .initialization import init_complex_matrix, build_nearly_identity, initialize_to_correct_model
-from .masking import init_weight_masks, apply_weight_masks
-from .losses import Complex_MSE_Loss, Batched_Complex_MSE_Loss, inverse_penalty
-from .precision_attention_block_old import PrecisionAttentionBlock, BatchedPrecisionAttentionBlock_v1
-from .precision_attn_block import BatchedPrecisionAttentionBlock
-from .misc_layers import HadamardLayer, TemporalNorm, TemporalWhiteningLayer
-from .networks import PrecisionNet_1layer, PrecisionNet
